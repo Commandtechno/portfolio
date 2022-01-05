36ae295ec8f3d8ac811eaca73ec162c7f90d0364
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2022-01-04 23:48:56 -0600

    gn

--- proxy.js
+++
@@ -1,9 +0,0 @@
-const { createServer } = require("http");
-const { get } = require("https");
-
-createServer((req, res) =>
-  get(new URL(req.url, "https://discord.com"), proxy => {
-    res.writeHead(proxy.statusCode, { ...proxy.headers, "access-control-allow-origin": "*" });
-    proxy.pipe(res);
-  })
-).listen(3002);
