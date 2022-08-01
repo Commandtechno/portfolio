f9f30ff02db7863a00a227bcb5995e29341ebda6
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2022-08-01 18:14:13 -0500

    enable no-sandbox

--- scripts/prebuild.ts
+++ scripts/prebuild.ts
@@ -4,7 +4,7 @@ import puppeteer from "puppeteer";
 import sharp from "sharp";
 
 console.log("Launching browser...");
-const browser = await puppeteer.launch();
+const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
 const page = await browser.newPage();
 await page.setViewport({ width: 1920, height: 1080 });
 await page.goto("file://" + resolve());
